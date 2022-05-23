3902164633c0a75f4fa148edb685a0275900302e
zoop1259 <kdm432191@gmail.com>
2022-05-23 21:03:05 +0900

    Initial Commit

---
+++ SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by 강대민 on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
